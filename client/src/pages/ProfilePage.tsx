20599d321f6acdba1783eba38e799e7a2d65dc0b
olathegreat <ademolarotimi2000@gmail.com>
2025-02-11 20:21:41 +0100

    feat

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -4,7 +4,7 @@ import { apiClient } from "../lib/apiClient";
 import { Input } from "../components/ui/input";
 import { IoIosAdd } from "react-icons/io";
 import { Button } from "../components/ui/button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toast } from "sonner";
 import { setUserDetails } from "../utils/appSlice";
 import CustomLoaderCircle from "../components/LoaderCircle";
@@ -22,7 +22,9 @@ const ProfilePage = () => {
   const [updateLoading, setUpdateLoading] = useState(false);
   const [getLoading, setGetLoading] = useState(false);
   const imageref = useRef<HTMLInputElement | null>(null);
+  const [imageChangeLoading, setImageChangeLoading] = useState(false);
   const dispatch = useDispatch();
+  const saveUserInfo = useSelector((state: any) => state.app.userDetails);
 
   useEffect(() => {
     const getUserInfo = async () => {
@@ -32,7 +34,8 @@ const ProfilePage = () => {
         console.log(res.data);
         setUserInfo(res.data);
         setPreviewImageUrl(res.data.picture);
-        console.log(previewImageUrl)
+        console.log(previewImageUrl);
+        dispatch(setUserDetails(res.data));
         setGetLoading(false);
       } catch (err) {
         console.log(err);
@@ -46,19 +49,22 @@ const ProfilePage = () => {
     imageref && imageref.current && imageref.current.click();
   };
 
-  const updateUserInfo = async () => {
-    setUpdateLoading(true);
+  const updateUserInfo = async (e: any) => {
+    e.preventDefault();
+    if (userInfo === saveUserInfo) {
+      toast("Change picture or fullname");
+      return;
+    }
 
     try {
+      setUpdateLoading(true);
       const updatedFormData = new FormData();
 
       updatedFormData.append("fullname", userInfo!.fullname);
 
-      updatedFormData.append("image", userInfo!.picture);
-
       const res = await apiClient.patch(
         "/api/v1/auth/update",
-        updatedFormData ,
+        updatedFormData,
         {
           headers: {
             "Content-Type": "multipart/form-data",
@@ -68,6 +74,7 @@ const ProfilePage = () => {
 
       console.log(res);
       dispatch(setUserDetails(res.data));
+      setUserInfo(res.data)
       toast.success("updated successfully");
       setUpdateLoading(false);
     } catch (err) {
@@ -76,35 +83,78 @@ const ProfilePage = () => {
     }
   };
 
+  const handleFileChange = async (e: any) => {
+    try {
+      setImageChangeLoading(true);
+      if (e.target.files && e.target.files[0]) {
+        const file = e.target.files[0];
+        const previewUrl = URL.createObjectURL(file);
+        setPreviewImageUrl(previewUrl);
+        setUserInfo({ ...userInfo!, picture: file });
+      }
+
+      setUpdateLoading(true);
+      const updatedFormData = new FormData();
+
+      updatedFormData.append("fullname", userInfo!.fullname);
+      updatedFormData.append("image", e.target.files[0]);
+
+      const res = await apiClient.patch(
+        "/api/v1/auth/update",
+        updatedFormData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log(res);
+      dispatch(setUserDetails(res.data));
+      toast.success("image update successful");
+    //   setUserInfo(res.data);
+      setUpdateLoading(false);
+      setImageChangeLoading(false);
+    } catch (err) {
+      setImageChangeLoading(false);
+      setUpdateLoading(false);
+      console.log(err);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center h-[100vh]">
-        {
-            getLoading && <CustomLoaderCircle/>
-        }
-      <div className={`max-w-[600px] p-10 h-fit bg-[#10002e] rounded-lg ${getLoading ? "hidden" : "flex"}  gap-10`}>
+      {getLoading && <CustomLoaderCircle />}
+      <div
+        className={`max-w-[600px] p-10 h-fit bg-[#10002e] rounded-lg ${
+          getLoading ? "hidden" : "flex"
+        }  gap-10`}
+      >
         {userInfo && (
           <div className="h-40 w-40 group relative">
-            <UserDisplayImage previewImage={previewImageUrl} user={userInfo} />
+            <UserDisplayImage  user={saveUserInfo} />
 
-            <div
-              onClick={handleFileInput}
-              className="absolute hidden group-hover:flex w-full h-full rounded-full bg-white/30 top-0  items-center justify-center transition-all duration-300 cursor-pointer"
-            >
-              <IoIosAdd className="text-8xl font-bold text-white" />
-            </div>
+            {!imageChangeLoading && (
+              <div
+                onClick={handleFileInput}
+                className="absolute hidden group-hover:flex w-full h-full rounded-full bg-black/30 top-0  items-center justify-center transition-all duration-300 cursor-pointer"
+              >
+                <IoIosAdd className="text-8xl font-bold text-white" />
+              </div>
+            )}
+            {imageChangeLoading && (
+              <div className="absolute  flex w-full h-full rounded-full bg-black/30 top-0  items-center justify-center transition-all duration-300 cursor-pointer">
+                <CustomLoaderCircle />
+              </div>
+            )}
             <input
               type="file"
               ref={imageref}
               name="image"
               accept=".png, .jpg, .jpeg, .svg, .webp"
               className="hidden"
-              onChange={(e) => {
-                if (e.target.files && e.target.files[0]) {
-                  const file = e.target.files[0];
-                  const previewUrl = URL.createObjectURL(file);
-                  setPreviewImageUrl(previewUrl);
-                  setUserInfo({ ...userInfo!, picture: file });
-                }
+              onChange={(e: any) => {
+                handleFileChange(e);
               }}
             />
           </div>
@@ -139,7 +189,11 @@ const ProfilePage = () => {
               className="bg-white text-purple-900 hover:bg-white/70 transition-all duration-300"
               onClick={updateUserInfo}
             >
-              {updateLoading ? <CustomLoaderCircle color="purple" /> : "Save Changes"}
+              {updateLoading ? (
+                <CustomLoaderCircle color="purple" />
+              ) : (
+                "Save Changes"
+              )}
             </Button>
           </form>
         </div>
